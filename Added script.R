f830353e1796955fcb204e7dabc8d3b16f103a31
mahonk89 <mahonk5692@gmail.com>
2022-12-06 22:45:17 -0800

    "Testing commit through Git BASH - takes a minute for the program to  be able to read the directory."  "This commit was uploaded to Github on 12/6/22 @ 10:46 PM PST"

---
+++ Added script.R
@@ -0,0 +1,8 @@
+print("Test add project to Github")
+
+print("I'm a little concerned because Git Bash didn't seem to work at all on 
+      the front end, but worked on the back end.")
+print("I tried adding through tools>shell and that seemed to work?")
+print("However once I needed to copy/paste code from Github into Git Bash,
+      that was successful")
+print("Error received: Failed to push some refs to [github repo link]")
